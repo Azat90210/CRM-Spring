a51ffd1328572f9b73d503ff85f825431def84ae
Mukhs <azat_market@mail.ru>
2023-03-12 21:35:33 +0300

    DataBase

---
+++ src/main/java/project/controllers/TermsController.java
@@ -0,0 +1,71 @@
+package project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import project.dao.TermDAO;
+import project.models.Term;
+
+@Controller
+@RequestMapping("/terms")
+public class TermsController {
+    private final TermDAO termDAO;
+
+    @Autowired
+    public TermsController(TermDAO termDAO) {
+
+        this.termDAO = termDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("terms", termDAO.index());
+        return "terms/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("term", termDAO.show(id));
+        return "terms/show";
+    }
+
+    @GetMapping("/new")
+    public String newTerm(@ModelAttribute("term") Term term) {
+        return "terms/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("term") Term term,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "terms/new";
+
+        termDAO.save(term);
+        return "redirect:/terms";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("term", termDAO.show(id));
+        return "terms/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Term term, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "terms/edit";
+
+        termDAO.update(id, term);
+        return "redirect:/terms";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        termDAO.delete(id);
+        return "redirect:/terms";
+    }
+
+}
---
+++ src/main/java/project/dao/TermDAO.java
@@ -0,0 +1,42 @@
+package project.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import project.models.Term;
+
+import java.util.List;
+
+@Component
+public class TermDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public TermDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Term> index() {
+        return jdbcTemplate.query("SELECT * FROM term", new BeanPropertyRowMapper<>(Term.class));
+    }
+
+    public Term show(int id) {
+        return jdbcTemplate.query("SELECT * FROM term WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Term.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Term term) {
+        jdbcTemplate.update("INSERT INTO term VALUES(1, ?, ?)", term.getName(), term.getDuration());
+    }
+
+    public void update(int id, Term updatedTerm) {
+        jdbcTemplate.update("UPDATE Person SET name=?, duration=? WHERE id=?", updatedTerm.getName(),
+                updatedTerm.getDuration(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM term WHERE id=?", id);
+    }
+
+}
